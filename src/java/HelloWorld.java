b82c9280f4c98f77243b112fa7e536cc3e781bf4
seokjun <iope100@naver.com>
2025-07-13 18:49:43 +0900

    sample

---
+++ src/java/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+  public static void main(String[] args) {
+    int a = 10;
+    int b = 20;
+    int c = a + b;
+
+    System.out.println("The result is: " + c);
+
+    for (int i = 0; i < 100; i++) {
+      System.out.println("Number: " + i);
+    }
+  }
+}
---
+++ src/javascript/sample.js
@@ -0,0 +1,13 @@
+function add(a, b) {
+  return a + b;
+}
+
+let x = 3;
+let y = 7;
+let z = add(x, y);
+
+console.log("Sum is " + z);
+
+for (let i = 0; i < 10; i++) {
+  console.log("Index: " + i);
+}
